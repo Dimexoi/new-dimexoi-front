85cdfbc9af6c2caa03fd538e97819887eef43f44
Aymerick Cotché <aymerickcotche@Aymerick.local>
2023-07-20 16:59:11 +0400

    get Sellsy product working

--- src/app/catalogue/page.tsx
+++ src/app/catalogue/page.tsx
@@ -9,7 +9,7 @@ import Footer from '@/components/Footer';
 
 import {getAllCollections, getCategoryList, getProductList, getSomeSdbProducts} from '../../services/sheets';
 import { setAllCategories, setAllCollections } from '@/redux/features/categorySlice';
-import { setAllProducts, setSomeSdbProducts } from '@/redux/features/productSlice';
+import { findAllProducts, setAllProducts, setSomeSdbProducts } from '@/redux/features/productSlice';
 
 import allCategoriesJson from '@/data/categories.json';
 import allProducts from '@/data/products.json';
@@ -45,7 +45,7 @@ export default function Catalogue() {
         }
         return randomIndexes;
     }
-
+    dispatch(findAllProducts());
     dispatch(setAllCategories(allCategoriesJson));
     dispatch(setAllCollections(allCollectionsJson));
 
--- src/redux/features/productSlice.ts
+++ src/redux/features/productSlice.ts
@@ -1,5 +1,7 @@
 import { createAction, createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+const Sellsy = require("node-sellsy").default
 
 export interface ProductState {
   allProducts : {
@@ -64,6 +66,43 @@ export interface ProductState {
   }
 }
 
+export const findAllProducts = createAsyncThunk('product/findAllProducts', async () => {
+  try {
+    console.log('here');
+      const sellsy = new Sellsy({
+        creds: {
+        consumerKey: process.env.NEXT_PUBLIC_CONSUMER_KEY,
+        consumerSecret: process.env.NEXT_PUBLIC_CONSUMER_SECRET,
+        userToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+        userSecret: process.env.NEXT_PUBLIC_TOKEN_SECRET,
+      },
+      endPoint: 'https://sistms-api.onrender.com'
+    });
+
+    
+    const params = {
+      type: "item",
+      search: {
+        categoryid: "218960",
+        tags: "website"
+      },
+      pagination: {
+        nbperpage: "10"
+      }
+    };
+
+    const data = await sellsy.api({
+      method: "Catalogue.getList",
+      params: params,
+    });
+
+    return data.response.result;
+  
+  } catch (error) {
+    console.log("error:", error);
+  }
+});
+
 // Define the initial state using that type
 const initialState: ProductState = {
   allProducts: [],
@@ -103,6 +142,38 @@ export const productSlice = createSlice({
     setDisplayedProduct: (state, action: PayloadAction<typeof initialState.displayedProduct>) => {
       state.displayedProduct = action.payload
     },
+  },
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder
+      .addCase(findAllProducts.fulfilled, (state, action) => {
+        console.log(action.payload);
+        for (const product of action.payload) {
+          state.someSdbProducts.push(
+            {
+              name: product.tradename,
+              categoryName: product.categoryName,
+              categorySlug: product,
+              subcategoryName: product,
+              attribute: product,
+              dimensions: product,
+              images: [...product],
+              description : product,
+              available: product,
+              collection: product,
+              collectionSlug: product,
+              slug: product,
+              subcategorySlug: product
+            }
+          )
+        }
+      })
+      .addCase(findAllProducts.pending, (state, action) => {
+        console.log('pending');
+      })
+      .addCase(findAllProducts.rejected, (state, action) => {
+        console.log('rejected');
+      })
   },
 })
 
